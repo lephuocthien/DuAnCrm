4aaccb8cceecfe5e1e3ddc0a96f97d765f79137f
Le Thien <doiden1999@gmail.com>
2020-10-14 22:47:59 +0700

    Update task folder
    Update add. edit, index

---
+++ src/com/myclass/controller/TaskController.java
@@ -0,0 +1,32 @@
+package com.myclass.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/task", "/task/add", "/task/edit" })
+public class TaskController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String action = req.getServletPath();
+		switch (action) {
+		case "/task":
+			req.getRequestDispatcher("/WEB-INF/views/task/index.jsp").forward(req, resp);
+			break;
+		case"/task/add":
+			req.getRequestDispatcher("/WEB-INF/views/task/add.jsp").forward(req, resp);
+			break;
+		case"/task/edit":
+			req.getRequestDispatcher("/WEB-INF/views/task/edit.jsp").forward(req, resp);
+			break;
+		}
+	}
+
+}
