21317ad41c203c0483fbd82f1cd2feeedfbd5fec
Le Thien <doiden1999@gmail.com>
2020-10-15 20:06:28 +0700

    Revert "Groupwork"
    This reverts commit 994095b26f7d963b7d51fce28a3aa7de9f79fe97.

--- src/com/myclass/controller/GroupworkController.java
+++ src/com/myclass/controller/GroupworkController.java
@@ -7,23 +7,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.myclass.dto.GroupworkDto;
-import com.myclass.service.GroupworkService;
-
 @WebServlet(urlPatterns = {"/groupwork","/groupwork/add","/groupwork/edit","/groupwork/details"})
 public class GroupworkController extends HttpServlet {
-	
 	private static final long serialVersionUID = 1L;
-	private GroupworkService groupworkService = null;
-	
-	public GroupworkController() {
-		groupworkService = new GroupworkService();
-	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		String action = req.getServletPath();
-		
 		switch (action) {
 		case "/groupwork":
 			req.getRequestDispatcher("/WEB-INF/views/groupwork/index.jsp").forward(req, resp);
@@ -37,41 +28,7 @@ public class GroupworkController extends HttpServlet {
 		case"/groupwork/details":
 			req.getRequestDispatcher("/WEB-INF/views/groupwork/details.jsp").forward(req, resp);
 			break;
-		default:
-			break;
 		}
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		String action = req.getServletPath();
-		
-		String name = req.getParameter("name");
-		String startDay = req.getParameter("startDay");
-		String endDay = req.getParameter("endDay");
-		int statusId = Integer.valueOf(req.getParameter("statusId"));
-		int userId = Integer.valueOf(req.getParameter("userId"));
-		int taskId = Integer.valueOf(req.getParameter("taskId"));
-		
-		GroupworkDto dto = new GroupworkDto();
-		dto.setName(name);
-		dto.setStartDay(startDay);
-		dto.setEndDay(endDay);
-		dto.setStatusId(statusId);
-		dto.setUserId(userId);
-		dto.setTaskId(taskId);
-		
-		
-		switch (action) {
-		case "/groupwork/add":
-			
-			break;
-
-		default:
-			break;
-		}
-		resp.sendRedirect(req.getContextPath() + "/groupwork");
 	}
 	
 }
