55fbc1d03530f928a1015deaefa7743beee0fac4
TrungVo131 <votantrung2k1@gmail.com>
2020-10-22 18:13:50 +0700

    Update2

--- src/com/myclass/filter/Utf8Filter.java
+++ src/com/myclass/filter/Utf8Filter.java
@@ -7,6 +7,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Utf8Filter implements Filter{
 
@@ -14,10 +16,16 @@ public class Utf8Filter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, 
 			FilterChain chain)
 			throws IOException, ServletException {
-		// --- CODE XỬ LÝ REQUEST
-		request.setCharacterEncoding("UTF-8");
+		// --- CODE Xá»¬ LĂ� REQUEST
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		
+		// --------------------------
+		
+		req.setCharacterEncoding("UTF-8");
 		chain.doFilter(request, response);
-		response.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
 	}
 
 }
